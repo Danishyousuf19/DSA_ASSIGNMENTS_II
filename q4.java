e56f56e5e135f4c93d35b9944e9ac454e587386c
danish yousuf <130566129+Danishyousuf19@users.noreply.github.com>
2023-04-21 17:56:08 +0530

    Update q4.java

--- q4.java
+++ q4.java
@@ -1,25 +1,27 @@
 import java.util.Scanner;
 
-/*Define a class called product. Two instance variables of the class are pid (product-id) and 
+
+/*Define a class called product. Two instance variables of the class are (product-id) and 
 price (product price). It consists of one static variable tot_price (total price). Initialize the value 
 of instance variables through parameterized constructor. It consists of a display ( ) method to 
 display the value of instance variables. A person went to market and purchase 5 different 
 products. Using the above mentioned class, display the details of products that the person has 
 purchased. Also, determine how much total amount the person will pay for the purchase of 5 
 products. */
-class negative extends Exception{
-   public String toString()	{
-   
-   return "*ERROR* entered negative number";
-}
+
+class negativenoo extends Exception{
+	public String getMessage() {
+		return "*ERROR* entered negative number";
+	}
+  
 }
 class product{
 	int id,price;
-	product(int id,int price)throws negative{
+	product(int id,int price)throws negativenoo{
 		this.id=id;
 		this.price=price;
 		if(id<=0|price<0) {
-			throw  new negative();
+			throw  new negativenoo();
 		}
 	}
 	void display()
@@ -28,25 +30,39 @@ class product{
 		System.out.println("price of one piece = "+this.price);
 	}
 	void total(int n) {
-		System.out.println("total price for "+n+" products = "+(n*this.price));
+		
+		System.out.println("total price for above  products = "+(n));
 	}
 }
 public class q4 {
 
 	public static void main(String[] args) {
 		Scanner sc=new Scanner(System.in);
-		System.out.println("enter id and price of product");
-		int x=sc.nextInt(),y=sc.nextInt();
-		try {
-			product vico =new product(x,y);
-			System.out.println("enter number of items");
-			int z=sc.nextInt();
-			vico.display();
-			vico.total(z);
-		} catch (negative e) {
-			// TODO Auto-generated catch block
-			System.out.println(e.toString());
-		}
+		
+		int sum=0;
+			product v[] =new product[5];
+			for (int i=0;i<5;i++) {
+				System.out.println("enter id and price of product");
+				int x=sc.nextInt(),y=sc.nextInt();
+				try {
+					v[i]=new product(x,y);
+					v[i].display();
+					sum+=v[i].price;
+					if(i==4) {
+				
+				product yy=new product(1,0);
+				yy.total(sum);
+			
+					}
+				} catch (negativenoo e) {
+					// TODO Auto-generated catch block
+					System.out.println(e.getMessage());
+					break;
+				}
+				
+			}
+			
+		
 	}
 
 }
